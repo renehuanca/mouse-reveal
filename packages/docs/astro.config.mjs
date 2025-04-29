81ff7a12643fce2e52cf29505cf8a2b8ca8686df
pixel grub <renehuanca999@gmail.com>
2025-04-29 18:10:06 -0400

    add base

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://renehuanca.github.io',
+  base: '/mouse-reveal/',
   vite: {
     plugins: [tailwindcss()]
   }
